84b452ee9695ab90b687f9983c62ecc758ed1dd3
velioterio <velioterio@dxc.com>
2019-09-17 12:02:04 -0300

    Subindo projeto de referência com Response Restful

---
+++ src/main/java/com/kazale/api/controllers/EmpresaController.java
@@ -0,0 +1,26 @@
+package com.kazale.api.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kazale.api.dtos.EmpresaDto;
+import com.kazale.api.responses.Response;
+
+@RestController
+@RequestMapping("/api/empresas")
+public class EmpresaController {
+
+	@PostMapping
+	public ResponseEntity<Response<EmpresaDto>> cadastrar(@RequestBody EmpresaDto empresaDto) {
+		Response<EmpresaDto> response = new Response<EmpresaDto>();
+		
+		empresaDto.setId(1L);
+		response.setData(empresaDto);
+		
+		return ResponseEntity.ok(response);
+	}
+
+}
